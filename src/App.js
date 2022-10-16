2dc31f3af1671303c3f2064556496b264d28bd17
patrikbenjaminn <patrik.kemppainen@gmail.com>
2022-10-16 17:59:06 +0300

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const URL = 'https://api.thecatapi.com/v1/images/search?api_key='
 
 function App() {
+  
+  const [kissa, setKissa] = useState('')
+  const [rotu, setRotu] = useState('')
+ 
+  function refreshPage() {
+    window.location.reload(false);
+  }
+  
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response)=>{
+      const kuva = response.data[0].url
+      //const rotu = response.data[0].breeds[0].name
+      
+      if (response.data[0].breeds.length == 1){
+      setRotu(response.data[0].breeds[0].name);}
+      else{
+        setRotu ("Maatiaiskissa");
+      }
+      setKissa(kuva)
+    }).catch(error => {
+      alert("Söpöä kissaa ei voida hakea")
+    })
+   }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mikä kissa olet tänään?
+      </h1>
+      <div>
+      <img src= {kissa} ></img>
+      </div>
+      <h3> {rotu}! </h3> 
+      <div>
+      <button onClick={refreshPage}>Kokeile uudelleen!</button>
+    </div>
     </div>
   );
 }
